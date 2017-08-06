ccca2a7a5f1c56d82bb877c113d3504efc5d24d7
Fabio Nolasco <gomidefabio@gmail.com>
2017-08-05 20:41:01 -0400

    [FN] Improve: made main url to load index.html

--- config.js
+++ config.js
@@ -21,5 +21,25 @@ module.exports = {
         projectId: 'fabionolasco-91850',
         storageBucket: 'fabionolasco-91850.appspot.com',
         messagingSenderId: '471822727829'
+    },
+    feed: {
+        title: 'Fabio Nolasco - Front End',
+        description: 'This is my personal feed!',
+        id: 'https://fabionolasco.com/',
+        link: 'https://fabionolasco.com/',
+        image: 'https://fabionolasco.com/image.png',
+        favicon: 'https://fabionolasco.com/favicon.ico',
+        copyright: 'All rights reserved 2013, John Doe',
+        updated: new Date(2013, 06, 14), // optional, default = today 
+        generator: 'awesome', // optional, default = 'Feed for Node.js' 
+        feedLinks: {
+            json: 'https://fabionolasco.com/json',
+            atom: 'https://fabionolasco.com/atom',
+        },
+        author: {
+            name: 'Fabio Nolasco',
+            email: 'gomidefabio' + '@gm' + 'ail.com',
+            link: 'https://fabionolasco.com/'
+        }
     }
 };
--- index.js
+++ index.js
@@ -1,41 +1,78 @@
 const EXPRESS = require('express');
 const FIREBASE = require('firebase');
+const FEED = require('feed')
 const APP = EXPRESS();
+const FS = require("fs");
+const PATH = require("path");
 const CONFIG = require('./config.js');
 
+// Start Firebase
 const FIRE = FIREBASE.initializeApp(CONFIG.firebaseAccount);
-
 const REF = {};
 
+// Start Feed Builded
+let feed = new FEED(CONFIG.feed)
+
+// Add Posts to Feed
 CONFIG.content.forEach((source) => {
-
     REF[source.url] = typeof REF[source.url] !== 'undefined' ? REF[source.url] : FIRE.database().ref(source.firebaseSource);
-
-    APP.get('/' + source.url, (req, res) => {
-        REF[source.url].orderByKey()
-            .limitToLast(source.numberRecords)
-            .once('value')
-            .then((snapshot) => {
-                const data = snapshot.val();
-                const keys = Object.keys(data)
-                const postsResult = []
-                keys.forEach((key) => {
-                    postsResult.push(data[key])
-                })
-                const result = {posts: postsResult.slice(0)};
-                res.send('<pre>' + JSON.stringify(result, null, 4) + '</pre>');
-                // res.send('<pre>' + Object.keys(blog) + '</pre>');
-            });
-    });
-
+    REF[source.url].orderByKey()
+        .limitToLast(source.numberRecords)
+        .once('value')
+        .then((snapshot) => {
+            const data = snapshot.val();
+            const keys = Object.keys(data)
+            const postsResult = []
+            keys.forEach((key) => {
+                postsResult.push(data[key])
+                addPost(data[key], source.url)
+            })
+        });
 });
-
+    
+// Build URLs
 APP.get('/', (req, res) => {
-    const result = [];
-    CONFIG.content.forEach((source) => {
-        result.push('<a href="/' + source.url +'">' + source.url + '</a>');
+    const filename = PATH.join(process.cwd(), 'index.html')
+    FS.readFile(filename, 'binary', function(err, file) {
+      if(err) {        
+        res.writeHead(500, {"Content-Type": "text/plain"});
+        res.write(err + "\n");
+        res.end();
+        return;
+      }
+      res.writeHead(200);
+      res.write(file, "binary");
+      res.end();
     });
-    res.send(result.join(' '));
+});
+APP.get('/rss', (req, res) => {
+    res.setHeader('content-type', 'application/xml');
+    res.send(feed.rss2());
+});
+APP.get('/atom', (req, res) => {
+    res.setHeader('content-type', 'application/xml');
+    res.send(feed.atom1());
 });
 
+// Add Posts to Feed Plugin
+function addPost(post, sourceUrl) {
+    feed.addItem({
+        title: post.title,
+        id: CONFIG.feed.link + sourceUrl + '/' + post.slug,
+        link: CONFIG.feed.link + sourceUrl + '/' + post.slug,
+        description: post.description.substring(0,300) + '...',
+        content: post.description,
+        author: [
+            {
+                name: CONFIG.feed.author.name,
+                email: CONFIG.feed.author.email,
+                link: CONFIG.feed.author.link
+            }
+        ],
+        date: new Date(),
+        image: post.url + post.imageName
+    })
+}
+
+// Start Listening
 APP.listen(CONFIG.port);
